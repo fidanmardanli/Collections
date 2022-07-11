2b6f5800a6c6905e0c5dc01f31d09e54ace6647c
fidanmardanli <fidannm@code.edu.az>
2022-07-12 00:27:02 +0400

    Collections

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//            List<int> nums = new List<int>();
+
+//            nums.Add(5);
+//            nums.Add(55);
+//            nums.Add(53);
+//            nums.Add(9);
+
+//            foreach (var item in nums)
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//            for (int i = 0; i < nums.Count; i++)
+//            {
+//                Console.WriteLine(nums[i]);
+
+//            }
+
+//            if (nums.Count == 4)
+//            {
+//                Console.WriteLine("List count is {0}", nums.Count);
+//            }
+
+//            List<Person> people = new List<Person>();
+//            Person person1 = new Person();
+//            person1.Name = "fidahs";
+
+//            peoople.Add(new Person { Id = 1, Name = "Orxan", Email = "orxan@gmail.comn" });
+//            GetStudents(people);
+//            GetNumbers(nums);
+
+//            var result = GetPersonByName(people, "orxan");
+//            Console.WriteLine(result.Name + "-" + result.Id + "_" + result.Email);
+
+//        }
+//        public static void GetStudents(List<Person> students)
+//        {
+//            foreach (var stu in students)
+//            {
+//                Console.WriteLine("Students ; Id - {0}, Name - {1}, Email - {2}", stu.Id, stu.Name, stu.Email);
+//            }
+//        }
+//        public static List<Person> GetPersonByName(List<Person> people, string name)
+//        {
+//            var person = people.Find(mbox => mbox.Name == name);
+//            return person;
+
+//        }
+
+//        Hashtable datas = new Hashtable();
+//        datas.Add(1, "Togrul");
+
+//        foreach (var item in datas.Values)
+//            {
+//            Console.WriteLine(item);
+//            }
+
+//    SortedList datas = new System.Collections.Generic.SortedList();
+//    datas.Add(1, Togrul);
+
+//        foreach (DictionaryEntry item in datas)
+//        {
+//        Console.WriteLine(item.Key + item.Value);
+//        }
+
+//    }
+
+//    class Person
+//    {
+
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public string Email { get; set; }
+
+//    }
+    
+
+//}
